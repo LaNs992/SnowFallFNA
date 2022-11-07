5d69a77ae64bfc1b1ecd857fcfe42420f15eadf7
LaNs992 <106981716+LaNs992@users.noreply.github.com>
2022-11-08 00:01:51 +0300

    Добавьте файлы проекта.

---
+++ GameEngine/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using GameEngine;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+static class Program
+{
+    /// <summary>
+    /// </summary>
+    static void Main(string[] args)
+    {
+        using (Game1 game = new Game1())
+        {
+            game.Run();
+        }
+    }
+}
